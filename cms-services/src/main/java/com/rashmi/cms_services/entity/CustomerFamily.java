56a4eaf6a29124667582c99ead232ec942dc15d9
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-05-02 02:50:07 +0530

    fix: add EqualsAndHashCode to CustomerFamily

--- cms-services/src/main/java/com/rashmi/cms_services/entity/CustomerFamily.java
+++ cms-services/src/main/java/com/rashmi/cms_services/entity/CustomerFamily.java
@@ -2,18 +2,23 @@ package com.rashmi.cms_services.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "customer_family")
 public class CustomerFamily {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id", nullable = false)
+    @JsonIgnore
     private Customer customer;
 
     @ManyToOne(fetch = FetchType.LAZY)
