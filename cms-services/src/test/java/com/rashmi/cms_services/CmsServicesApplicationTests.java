74c2314571d9a33a5478ab35594f894c424f34b4
Rashmi Rathnayake <148036626+rashdiwsl@users.noreply.github.com>
2026-05-01 18:13:52 +0530

    chore: initialize Spring Boot project structure

---
+++ cms-services/src/test/java/com/rashmi/cms_services/CmsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashmi.cms_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
